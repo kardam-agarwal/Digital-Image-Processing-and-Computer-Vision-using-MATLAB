f52bf9c03d26c1d9c13bdeeebd59d4e24b0f54d0
Kardam Agarwal <60712504+kardam27@users.noreply.github.com>
2020-07-07 13:13:34 +0530

    Update and rename Face and Nose detection.txt to Face and Nose detection.m

--- Face and Nose detection.m
+++ Face and Nose detection.m
@@ -1,25 +1,26 @@
 facedetector=vision.CascadeObjectDetector();
 videofilereader=vision.VideoFileReader('video1.mp4');
-videoframe=step(videofilereader);                           #since frames are added in stack, face has to be detected as the frame changes#
+videoframe=step(videofilereader);                           %Since frames are added in stack, face has to be detected as the frame changes
 bbox=step(facedetector,videoframe);
 videoout=insertObjectAnnotation(videoframe,'rectangle',bbox,'face');
 imshow(videoout);
-[huechannel,~,~]=rgb2hsv(videoframe);#Hsv is better for detecting skin tones#
+[huechannel,~,~]=rgb2hsv(videoframe);                       %Hsv is better for detecting skin tones
 figure;
 imshow(huechannel);
 rectangle('Position',bbox(1,:),'LineWidth',2,'EdgeColor',[1 1 0]);
-nosedetector=vision.CascadeObjectDetector('Nose','UseROI',true);     #UseRoi-Region of interest ,used when we need to focus on a particular boundary and need to process it . By default ,false#
+nosedetector=vision.CascadeObjectDetector('Nose','UseROI',true);     %UseRoi-Region of interest ,used when we need to focus on a particular boundary and need to process it. By default,false
+
 nosebbox=step(nosedetector,videoframe,bbox(1,:));
 tracker=vision.HistogramBasedTracker();
 
-#For simultaneously detecting face and nose#
+%For simultaneously detecting face and nose
 
 initializeObject(tracker,huechannel,nosebbox(1,:));
 Videoinfo=info(videofilereader);
-videoplayer=vision.VideoPlayer('Position',[300 300 Videoinfo.VideoSize+30]);      #Videosize + 30 , for creating margin#
+videoplayer=vision.VideoPlayer('Position',[300 300 Videoinfo.VideoSize+30]);    %Videosize + 30 , for creating margin
 while ~isDone(videofilereader)
     videoframe=step(videofilereader);
-    [huechannel,~,~]#to black and white#=rgb2hsv(videoframe);
+    [huechannel,~,~] =rgb2hsv(videoframe);                                      %to black and white%
     bbox=step(tracker,huechannel);
     videoout=insertObjectAnnotation(videoframe,'rectangle',bbox,'face');
     step(videoplayer,videoout);
